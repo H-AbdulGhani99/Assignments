52b7a635b39e1f10ba67601c7b8559f4a1cf1caa
H. Abdul Ghani <73195716+H-AbdulGhani99@users.noreply.github.com>
2023-12-02 22:04:45 +0500

    Add files via upload

--- a1.ts
+++ a1.ts
@@ -1,20 +1,10 @@
-//- Scenario 1 - Buying Groceries:
-//- Simulate buying fruits, vegetables, and groceries.
-//- Apply if-else statements to check specific conditions:
-//- Check if the customer wants fruits or vegetables.
-//- Based on the choice, suggest different items available and their prices.
-//- Display the total bill.
-
 import inquirer from "inquirer";
-import clear from "clear";
-// import util from 'node:util';
-
+import promptSync from "prompt-sync";
 let groceriesPrices = [200, 150, 120, 30, 80, 300, 220];
 const fruitPrices = [200, 150, 120, 30, 80, 300, 220];
 const vegetablePrices = [200, 150, 120, 30, 80, 300, 220];
 let items: string[] = [];
 let itemPrices: number[] = [];
-
 const fruits = [
   "Mango",
   "Apple",
@@ -38,13 +28,12 @@ function line(): void {
   console.log("******************************");
   console.log("******************************");
 }
-// ***************************************************************************
 let count = 0;
 function showmenu(): void {
   console.log("1.For Fruits");
   console.log("2.For Vegetables");
   console.log("3.For Groceries");
-  console.log("4.To Exit");
+  console.log("4.To Exit and Print Bill");
 }
 async function menuProcessing() {
   let getinput;
@@ -66,8 +55,6 @@ async function menuProcessing() {
     }
   } while (!(getinput == 4)); // to make the loop running
 }
-// ***************************************************************************/
-//*************************************************************************** */
 function showingAllFruits(): void {
   line();
   console.log("S.R\t\tFruits \t\t Prices");
@@ -96,9 +83,6 @@ function showingAllGrocereies(): void {
     );
   }
 }
-//***************************************************************************** */
-
-////////////////////////////////////////////////////////
 // for Taking Input
 async function takingUserInput() {
   let input;
@@ -120,14 +104,13 @@ async function takingUserInput() {
   } while (!validateinput(input.userInput));
 }
 function validateinput(input: any) {
+  //This function is used for validating inputs
   if (isNaN(input) || input <= 0) {
     return false;
   } else {
     return true;
   }
 }
-
-/////////////////////////////////////////////////////////
 function addtoCart(selection: number, getinput: number): void {
   //This function add the items in the cart
   if (getinput == 1) {
@@ -156,16 +139,14 @@ function generateBill() {
     }
     console.log(`\t\t\t\t\tTotal : ${sum}`);
     if (sum > 2000) {
-      sum - (sum / 100) * 5; // Calculating Discount
-      console.log(`You Got 5% discount `);
-      console.log(`\t\t\t\t\tTotal : ${sum}`);
+      let discount = (sum / 100) * 5; // Calculating Discount
+      console.log(`\t\t\t\t\tDiscount : ${discount}`);
+      console.log(`\t\t\t\t\tTotal : ${sum - discount}`);
     }
     paymentMethod();
   }
 }
-// #############################################################
 async function fruitsProcessing() {
-  ``;
   do {
     const constant = 1;
     showingAllFruits();
@@ -177,7 +158,7 @@ async function fruitsProcessing() {
     } else {
       console.log("Only Enter the given Serial numbers");
     }
-  } while ((await asking()) == true);
+  } while (asking() == true);
 }
 async function vegetableProcessing() {
   do {
@@ -191,12 +172,12 @@ async function vegetableProcessing() {
     } else {
       console.log("Only Enter the given Serial numbers");
     }
-  } while ((await asking()) == true);
+  } while (asking() == true);
 }
 async function groceriesProcessing() {
-  showingAllGrocereies();
   do {
     const constant = 3;
+    showingAllGrocereies();
     let selection = Number(await takingUserInput());
     selection--;
     if (selection > 1 && selection <= fruits.length) {
@@ -205,78 +186,68 @@ async function groceriesProcessing() {
     } else {
       console.log("Only Enter the given Serial numbers");
     }
-  } while ((await asking()) == true);
+  } while (asking() == true);
 }
-// #############################################################
-
-/////////////////////////////////////////////////////
-async function getStringInput() {
-  let userInput;
-  userInput = await inquirer.prompt([
-    {
-      name: "StringInput",
-      type: "input",
-      message: "Enter yes or no (y/n)",
-    },
-  ]);
-  return userInput.StringInput;
+function getStringInput() {
+  const prompt = promptSync();
+  let name = prompt("Enter yes or no (y/n) ");
+  return name;
 }
-
-async function paymentMethod() {
+function paymentMethod() {
   let payment = ["Online", "Cash on Delivery", "Card"];
   let onlineMethods = ["Mobile App", "EasyPaisa", "JazzCash"];
   let Cards = ["Visa Card", "Master Card ", "Debit Card", "Credit Card"];
 
   for (let count: number = 0; count < payment.length; count++) {
     console.log(`Do you pay by ${payment[count]} method `);
-    let bool: boolean = false;
-    let temporary = await asking();
+    let temporary = asking();
     if (temporary == true && payment[count] == "Online") {
       for (let counter in onlineMethods) {
         console.log(`Do you pay by ${onlineMethods[counter]}`);
-        let question = await asking();
+        let question = asking();
         if (question == true) {
           console.log(
             `Thank You for using ${payment[count]} Payment method using ${onlineMethods[counter]}`
           );
-          bool = true;
+          return;
+          // bool = true;
         }
-        break;
       }
     }
-    if (bool == false) {
-      if (temporary == true && payment[count] == "Card") {
-        for (let counter in Cards) {
-          console.log(`Do you pay by ${Cards[counter]}`);
-          let question = await asking();
-          if (question == true) {
-            console.log(
-              `Thank You for using ${payment[count]} Payment method using ${Cards[counter]}`
-            );
-          }
-          break;
+
+    if (temporary == true && payment[count] == "Card") {
+      for (let counter in Cards) {
+        console.log(`Do you pay by ${Cards[counter]}`);
+        let question = asking();
+        if (question == true) {
+          console.log(
+            `Thank You for using ${payment[count]} Payment method using ${Cards[counter]}`
+          );
+          return;
         }
       }
-      if (temporary == true && payment[count] == "Cash on Delivery") {
-        console.log(`Thank you for using for using ${payment[count]} method`);
-        break;
-      }
+    }
+    if (temporary == true && payment[count] == "Cash on Delivery") {
+      console.log(`Thank you for using for using ${payment[count]} method`);
+      return;
+    }
+  }
+}
+function asking() {
+  let count: number;
+  do {
+    let isContinue: string = String(getStringInput());
+    count = 0;
+    if (isContinue == "yes" || isContinue == "y") {
+      return true;
+    }
+    if (isContinue == "n" || isContinue == "no") {
+      return false;
     } else {
-      process.exit();
+      console.log("Wrong Value");
     }
-  }
+  } while (count == 0);
 }
-async function asking() {
-  let isContinue: string = String(await getStringInput());
-  if (isContinue == "yes" || isContinue == "y") {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-///////////////////////////////////////////////////////////////////////////
-
 line();
 console.log(`Welcome to Shopping Simulation`);
 line();
